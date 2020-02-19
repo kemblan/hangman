171a953f65b5f7568b4f13a7db34e76109fd737d
Tad <tchelkow@macbook-pro-2.eur.adobe.com>
2020-02-19 16:04:42 +0100

    wymiana jezyka fix #1

--- hangman.py
+++ hangman.py
@@ -1,10 +1,10 @@
-#importing the time module
+#Zmiana importing the time module
 import time
 
 #welcoming the user
 name = raw_input("What is your name? ")
 
-print "Hello, " + name, "Time to play hangman!"
+print "Witaj, " + name, "Time to play hangman!"
 
 print ""
 
