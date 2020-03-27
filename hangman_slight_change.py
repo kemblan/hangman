00461023eeea5e66255ded081b5d60a6bdd681a4
EC2 Default User <ec2-user@ip-172-31-46-41.ec2.internal>
2020-03-27 14:54:53 +0000

    Fixed spelling in the file

--- hangman_slight_change.py
+++ hangman_slight_change.py
@@ -1,21 +1,21 @@
-#Zmina importing the time module
+#Zmiana importing the time module
 import time
 
 #welcoming the user
-name = raw_input("Guten Tag")
+name = raw_input("What is your name? ")
 
-print "Hallo," + name, "es ist Zeit um Hangman zu Spielen"
+print "Witaj, " + name, "Time to play hangman!"
 
 print ""
 
 #wait for 1 second
 time.sleep(1)
 
-print "Lasst uns beginnen ..."
-time.sleep(0.6)
+print "Start guessing..."
+time.sleep(0.8)
 
 #here we set the secret
-word = "Geheimnis"
+word = "villian"
 
 #creates an variable with an empty value
 guesses = ''
@@ -26,59 +26,59 @@ turns = 10
 # Create a while loop
 
 #check if the turns are more than zero
-while turns > 0:
+while turns > 0:         
 
     # make a counter that starts with zero
-    failed = 0
+    failed = 0             
 
-    # for every character in secret_word
-    for char in word:
+    # for every character in secret_word    
+    for char in word:      
 
     # see if the character is in the players guess
-        if char in guesses:
-
+        if char in guesses:    
+    
         # print then out the character
-            print char,
+            print char,    
 
         else:
-
+    
         # if not found, print a dash
-            print "_",
-
+            print "_",     
+       
         # and increase the failed counter with one
-            failed += 1
+            failed += 1    
 
     # if failed is equal to zero
 
     # print You Won
-    if failed == 0:
-        print "Du hast Gewinnt !!!"
+    if failed == 0:        
+        print "You won"  
 
     # exit the script
-        break
+        break              
 
     print
 
     # ask the user go guess a character
-    guess = raw_input("adivina más:")
+    guess = raw_input("guess a character:") 
 
     # set the players guess to guesses
-    guesses += guess
+    guesses += guess                    
 
     # if the guess is not found in the secret word
-    if guess not in word:
-
+    if guess not in word:  
+ 
      # turns counter decreases with 1 (now 9)
-        turns -= 1
-
+        turns -= 1        
+ 
     # print wrong
-        print "falsch"
-
+        print "Wrong"    
+ 
     # how many turns are left
-        print "so viele:", + turns, 'links abbiegen' 
-
+        print "You have", + turns, 'more guesses' 
+ 
     # if the turns are equal to zero
-        if turns == 0:
-
+        if turns == 0:           
+    
         # print "You Lose"
-            print ""
+            print "You Lose" 
