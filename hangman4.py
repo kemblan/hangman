fa320b16410122b100c1b579330aa5b135739bce
EC2 Default User <ec2-user@ip-172-31-46-41.ec2.internal>
2020-03-27 14:37:43 +0000

    close #29

---
+++ hangman4.py
@@ -0,0 +1,84 @@
+#Zmina importing the time module
+import time
+
+#welcoming the user
+name = raw_input("Guten Tag")
+
+print "Halo," + name, "es ist Zeit um Hangman zu Spielen"
+
+print ""
+
+#wait for 1 second
+time.sleep(1)
+
+print "Lasst uns beginnen ..."
+time.sleep(0.5)
+
+#here we set the secret
+word = "Geheimnis"
+
+#creates an variable with an empty value
+guesses = ''
+
+#determine the number of turns
+turns = 10
+
+# Create a while loop
+
+#check if the turns are more than zero
+while turns > 0:
+
+    # make a counter that starts with zero
+    failed = 0
+
+    # for every character in secret_word
+    for char in word:
+
+    # see if the character is in the players guess
+        if char in guesses:
+
+        # print then out the character
+            print char,
+
+        else:
+
+        # if not found, print a dash
+            print "_",
+
+        # and increase the failed counter with one
+            failed += 1
+
+    # if failed is equal to zero
+
+    # print You Won
+    if failed == 0:
+        print "Du hast Gewinnt !!!"
+
+    # exit the script
+        break
+
+    print
+
+    # ask the user go guess a character
+    guess = raw_input("adivina más:")
+
+    # set the players guess to guesses
+    guesses += guess
+
+    # if the guess is not found in the secret word
+    if guess not in word:
+
+     # turns counter decreases with 1 (now 9)
+        turns -= 1
+
+    # print wrong
+        print "falsch"
+
+    # how many turns are left
+        print "so viele:", + turns, 'links abbiegen' 
+
+    # if the turns are equal to zero
+        if turns == 0:
+
+        # print "You Lose"
+            print ""
