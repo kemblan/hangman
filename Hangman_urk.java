b005b9f99d93c6b191d3ea4eb24ddb10f5a56e74
EC2 Default User <ec2-user@ip-172-31-7-245.us-east-2.compute.internal>
2020-04-04 20:46:20 +0000

    closes #29

---
+++ Hangman_urk.java
@@ -0,0 +1,131 @@
+import java.util.Scanner;
+
+public class Hangman {
+
+	private static String[] words = {"термінотор", "банан", "комп'ютер", "корова", "дощ", "вода" };
+	private static String word = words[(int) (Math.random() * words.length)];
+	private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+	private static int count = 0;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (count < 7 && asterisk.contains("*")) {
+			System.out.println("Впізнай слово");
+			System.out.println(asterisk);
+			String guess = sc.next();
+			hang(guess);
+		}
+		sc.close();
+	}
+
+	public static void hang(String guess) {
+		String newasterisk = "";
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == guess.charAt(0)) {
+				newasterisk += guess.charAt(0);
+			} else if (asterisk.charAt(i) != '*') {
+				newasterisk += word.charAt(i);
+			} else {
+				newasterisk += "*";
+			}
+		}
+
+		if (asterisk.equals(newasterisk)) {
+			count++;
+			hangmanImage();
+		} else {
+			asterisk = newasterisk;
+		}
+		if (asterisk.equals(word)) {
+			System.out.println("Правильно,ти переміг! твоє слов  " + word);
+		}
+	}
+
+	public static void hangmanImage() {
+		if (count == 1) {
+			System.out.println("Тобі не пощастило, спробуй знову!");
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("___|___");
+			System.out.println();
+		}
+		if (count == 2) {
+			System.out.println("Тобі не пощастило, спробуй знову!");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 3) {
+			System.out.println("Тобі не пощастило, спробуй знову!");
+			System.out.println("   ____________");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (count == 4) {
+			System.out.println("Тобі не пощастило, спробуй знову!");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 5) {
+			System.out.println("Тобі не пощастило, спробуй знову!")
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 6) {
+			System.out.println("Тобі не пощастило, спробуй знову!");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+		}
+		if (count == 7) {
+			System.out.println("ТИ ПРОГРАВ!");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |          _|_");
+			System.out.println("   |         / | \\");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+			System.out.println("ТИ ПРОГРАВ! ПРАВИЛЬНЕ СЛОВО  " + word);
+		}
+	}
+}
